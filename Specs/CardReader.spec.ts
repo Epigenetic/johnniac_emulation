d5bbdaa2c8cf3c046d9e300ecc103b61acf9c278
Jonah Shafran <jonahshafran@gmail.com>
2024-03-31 07:13:21 -0500

    Unit test CardReader

---
+++ Specs/CardReader.spec.ts
@@ -0,0 +1,24 @@
+import { CardReader } from "../Src/CardReader"
+
+describe("CardReader", () => {
+    it("Splits card into two words", () => {
+        const cardReader = new CardReader();
+        cardReader.setCard(0, (123n << 40n) | 456n);
+
+        const [firstWord, secondWord] = cardReader.nextCard();
+        expect(firstWord).toBe(123n);
+        expect(secondWord).toBe(456n);
+    });
+    it("Advances to next card", () => {
+        const cardReader = new CardReader();
+        cardReader.setCard(0, (123n << 40n) | 456n);
+        cardReader.setCard(1, (789n << 40n) | 135n);
+
+        const [firstWord, secondWord] = cardReader.nextCard();
+        expect(firstWord).toBe(123n);
+        expect(secondWord).toBe(456n);
+        const [thirdWord, fourthWord] = cardReader.nextCard();
+        expect(thirdWord).toBe(789n);
+        expect(fourthWord).toBe(135n);
+    })
+});
