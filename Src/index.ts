71f0f49d81a836ffcccc499ea469b4ef7033a8ee
Jonah Shafran <jonahshafran@gmail.com>
2025-01-11 19:37:49 -0600

    Hook typewriter into the site

--- Src/index.ts
+++ Src/index.ts
@@ -1,4 +1,13 @@
+import { JOSSTypewriter } from "./JOSSTypewriter.js";
+
+declare global {
+    var typewriter: JOSSTypewriter | undefined;
+}
+
 const typewriterWorker = new SharedWorker("/Src/MTC/TypewriterWorker.js", { type: "module" });
 typewriterWorker.port.start();
 const jossWorker = new Worker("/Src/Programs/JOSSWorker.js", { type: "module" });
 jossWorker.postMessage({ port: typewriterWorker.port }, [typewriterWorker.port]);
+
+globalThis.typewriter = new JOSSTypewriter(0, document.getElementById("jossInput")! as HTMLDivElement);
+globalThis.typewriter.online();
